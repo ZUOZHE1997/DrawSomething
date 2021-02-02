4327801c14fc950fe2c4e64366f6614c515082c4
liulin <2240379431@qq.com>
2021-02-02 13:54:18 +0800

    完善canvas画板

--- frontend/src/views/Content/index.js
+++ frontend/src/views/Content/index.js
@@ -1,15 +1,28 @@
 import React from 'react'
 import './index.css'
-import { Card, Button, Slider } from 'antd'
+import { Card, Button, Slider, Tooltip } from 'antd'
 import { Draw } from './draw'
 
 export class DrawContent extends React.Component {
-  componentDidMount() {
-    let draw = new Draw('draw')
-    // console.log(draw)
-    draw.init()
+  constructor() {
+    super()
+    this.draw = ''
+    this.state = {
+      disabled: true,
+    }
   }
-
+  componentDidMount() {
+    this.draw = new Draw({ element: 'draw' })
+    this.draw.init()
+    this.setState({
+      disabled: false,
+    })
+  }
+  changeLineWidth(value) {
+    if (this.draw) {
+      this.draw.changeLine(value)
+    }
+  }
   render() {
     const silderStyle = {
       display: 'inline-block',
@@ -22,17 +35,57 @@ export class DrawContent extends React.Component {
         <div className="draw-content">
           <canvas width="600" height="500" id="draw" />
           <div className="operating-btn-list">
-            <Button type="link" size="small">
-              橡皮擦
+            {}
+            <Button
+              type="link"
+              size="small"
+              onClick={() => {
+                this.draw.checkPen()
+              }}
+            >
+              画笔
             </Button>
-            <Button type="link" size="small">
+            <Tooltip title="有BUG,暂时不要用" color="red">
+              <Button
+                type="link"
+                size="small"
+                onClick={() => {
+                  this.draw.cleaning()
+                }}
+              >
+                橡皮擦
+              </Button>
+            </Tooltip>
+
+            <Button
+              type="link"
+              size="small"
+              onClick={() => {
+                this.draw.undo()
+              }}
+            >
               撤销
             </Button>
-            <Button type="link" size="small">
+            <Button
+              type="link"
+              size="small"
+              onClick={() => {
+                this.draw.reset()
+              }}
+            >
               重绘
             </Button>
             <div style={silderStyle}>
-              <Slider vertical defaultValue={30} />
+              <Slider
+                vertical
+                disabled={this.state.disabled}
+                defaultValue={3}
+                onAfterChange={(value) => {
+                  this.changeLineWidth(value)
+                }}
+                max={12}
+                min={2}
+              />
             </div>
           </div>
         </div>
--- frontend/src/views/MsgBoard/index.js
+++ frontend/src/views/MsgBoard/index.js
@@ -1,30 +1,32 @@
 import React from 'react'
 import './index.css'
-import { Comment, Avatar, Button, Input } from 'antd'
-
+import { Button, Input } from 'antd'
+import {CommentGuide} from '../../components/comment/index'
 const { TextArea } = Input
 
 export class MsgBoard extends React.Component {
-  // constructor(props) {
-  //     super(props);
-  // }
-
   render() {
+    const list = [
+      {
+        name: 'aloha',
+        avatar:
+          'https://avatars.githubusercontent.com/u/36844445?s=400&u=5d764d41119d96e18e0468bb5f22495db2d9882c&v=4',
+        content: 'helloworld',
+      },
+      {
+        name: 'tom',
+        avatar:
+          'https://avatars.githubusercontent.com/u/36844445?s=400&u=5d764d41119d96e18e0468bb5f22495db2d9882c&v=4',
+        content: 'helloworld',
+      },
+    ]
     return (
       <div className="message-board">
         <div className="message-board-show">
-          <Comment
-            author="Han Solo"
-            avatar={
-              <Avatar
-                src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
-                alt="Han Solo"
-              />
-            }
-            content={<p>We supply a series of design principles</p>}
-          />
+          {list.map((p) => {
+            return <CommentGuide item={p} key={p.name}></CommentGuide>
+          })}
         </div>
-
         <TextArea showCount={true} maxLength={40} autoSize={{ minRows: 4 }} />
         <Button type="primary" block>
           提交
