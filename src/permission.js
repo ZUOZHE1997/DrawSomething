9ba211dd59a1698b6d0e7cb8fe8c66dffea65505
liulin <2240379431@qq.com>
2021-03-01 17:22:59 +0800

    添加预览

--- src/permission.js
+++
@@ -1,7 +0,0 @@
-import router from "./router";
-import store from "./store";
-
-router.beforeEach(to => {
-  if (to.path === "/login") return true;
-  else if (store.state.name === "") return { path: "/login" };
-});
